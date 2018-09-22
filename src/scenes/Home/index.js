77d0294341191388082d6615022916e500f91b03
João Paulo Barbosa <jpaulobneto@gmail.com>
2018-09-22 20:05:17 -0300

    Use React Bootstrap Grid

--- src/scenes/Home/index.js
+++ src/scenes/Home/index.js
@@ -1,15 +1,32 @@
 import React from 'react';
+import {
+  Button, Col, Grid, Row,
+} from 'react-bootstrap';
 
 const Home = () => (
-  <React.Fragment>
-    <header>
-      <h1>Getting Started with Redux</h1>
-      <h2>Course examples</h2>
-    </header>
-    <main>
-
-    </main>
-  </React.Fragment>
+  <Grid>
+    <Row>
+      <Col xs={12}>
+        <header>
+          <h1>Getting Started with Redux</h1>
+          <h2>Course examples</h2>
+        </header>
+      </Col>
+    </Row>
+    <Row>
+      <Col xs={12}>
+        <main>
+          <ol>
+            <li>
+              Counter:
+              {' '}
+              <Button>Load</Button>
+            </li>
+          </ol>
+        </main>
+      </Col>
+    </Row>
+  </Grid>
 );
 
 export default Home;
