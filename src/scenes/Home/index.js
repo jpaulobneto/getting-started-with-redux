5b1d67ae5003ab454a453c69a04d54f1d135a785
João Paulo Barbosa <jpaulobneto@gmail.com>
2018-09-22 18:47:32 -0300

    Create the Home scene

---
+++ src/scenes/Home/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Home = () => (
+  <React.Fragment>
+    <header>
+      <h1>Getting Started with Redux</h1>
+      <h2>Course examples</h2>
+    </header>
+    <main>
+
+    </main>
+  </React.Fragment>
+);
+
+export default Home;
