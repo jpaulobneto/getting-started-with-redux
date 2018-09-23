d5eff25a1e43728e34ba33d1f36ba329cff1cc57
João Paulo Barbosa <jpaulobneto@gmail.com>
2018-09-23 02:34:30 -0300

    Implement TodoList and AddTodo button

--- src/scenes/TodoAppExample/index.js
+++ src/scenes/TodoAppExample/index.js
@@ -1,16 +1,51 @@
 import React from 'react';
-import { Col, Grid, Row } from 'react-bootstrap';
+import PropTypes from 'prop-types';
+import {
+  Button, Col, Grid, Row,
+} from 'react-bootstrap';
 
-const TodoApp = () => (
-  <Grid>
-    <Row>
-      <Col xs={12}>
-        <header>
-          <h1>TodoApp Example</h1>
-        </header>
-      </Col>
-    </Row>
-  </Grid>
-);
+class TodoApp extends React.Component {
+  render() {
+    const { todos, onAddTodo } = this.props;
+
+    return (
+      <Grid>
+        <Row>
+          <Col xs={12}>
+            <header>
+              <h1>TodoApp Example</h1>
+            </header>
+            <main>
+              <input
+                ref={(node) => {
+                  this.input = node;
+                }}
+              />
+              <Button bsStyle="primary" onClick={() => onAddTodo(this.input.value)}>
+                Add todo
+              </Button>
+              <ul>
+                {todos.map(todo => (
+                  <li key={todo.id}>{todo.text}</li>
+                ))}
+              </ul>
+            </main>
+          </Col>
+        </Row>
+      </Grid>
+    );
+  }
+}
+
+TodoApp.propTypes = {
+  todos: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      text: PropTypes.string.isRequired,
+      completed: PropTypes.bool.isRequired,
+    }),
+  ).isRequired,
+  onAddTodo: PropTypes.func.isRequired,
+};
 
 export default TodoApp;
