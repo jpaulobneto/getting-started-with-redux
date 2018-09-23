2e67fc6fe189dec79bd10e7fabf291b229862f55
João Paulo Barbosa <jpaulobneto@gmail.com>
2018-09-23 01:51:50 -0300

    Add todoApp reducer in store

--- src/reducer.js
+++ src/reducer.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
 import counter from './scenes/CounterExample/reducer';
+import todoApp from './scenes/TodoAppExample/reducer';
 
 export default combineReducers({
   counter,
+  todoApp,
 });
