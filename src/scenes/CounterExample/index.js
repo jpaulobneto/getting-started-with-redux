56bdd2aa8c44ba505e24c937c207e2bc83a77842
João Paulo Barbosa <jpaulobneto@gmail.com>
2018-09-23 00:19:30 -0300

    Use PropTypes dependency

--- src/scenes/CounterExample/index.js
+++ src/scenes/CounterExample/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import {
   Button, Col, Grid, Row,
 } from 'react-bootstrap';
@@ -14,5 +15,11 @@ const CounterExample = ({ value, onIncrement, onDecrement }) => (
     </Row>
   </Grid>
 );
+
+CounterExample.propTypes = {
+  onDecrement: PropTypes.func.isRequired,
+  onIncrement: PropTypes.func.isRequired,
+  value: PropTypes.number.isRequired,
+};
 
 export default CounterExample;
--- src/scenes/Root/index.js
+++ src/scenes/Root/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import PropTypes from 'prop-types';
 import CounterExample from '../containers/CounterExample';
 import Home from '../Home';
 
@@ -14,5 +15,9 @@ const Root = ({ store }) => (
     </Router>
   </Provider>
 );
+
+Root.propTypes = {
+  store: PropTypes.shape({}).isRequired,
+};
 
 export default Root;
