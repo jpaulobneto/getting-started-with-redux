9737b58162bca0b2beec5d576916ed954ee375d4
João Paulo Barbosa <jpaulobneto@gmail.com>
2018-09-23 17:29:54 -0300

    Persist the state in the local storage

--- src/store.js
+++ src/store.js
@@ -1,7 +1,20 @@
 import { createStore } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly';
+import throttle from 'lodash/throttle';
 import reducer from './reducer';
+import { loadState, saveState } from './services/localStorage';
 
-const store = createStore(reducer, composeWithDevTools());
+const persistedState = loadState();
+const store = createStore(reducer, persistedState, composeWithDevTools());
+
+store.subscribe(
+  throttle(() => {
+    saveState({
+      todoApp: {
+        todos: store.getState().todoApp.todos,
+      },
+    });
+  }, 1000),
+);
 
 export default store;
